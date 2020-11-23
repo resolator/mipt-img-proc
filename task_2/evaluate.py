72b0e4748db5f5c5f6cb50e41c6067564e6ea820
Vladyslav <v.dusiak@gmail.com>
2020-11-23 10:40:49 +0300

    Evaluation script updated for task 2

--- task_2/evaluate.py
+++ task_2/evaluate.py
@@ -5,14 +5,17 @@ import cv2
 import argparse
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+from pathlib import Path
 
 
 def get_args():
     """Arguments parser."""
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--src-img',
+    parser.add_argument('--src-img', type=Path, required=True,
                         help='Path to source image.')
-    parser.add_argument('--dst-img',
+    parser.add_argument('--dst-img', type=Path, nargs='+', required=True,
                         help='Path to generated image.')
 
     return parser.parse_args()
@@ -22,20 +25,29 @@ def main():
     """Application entry point."""
     args = get_args()
 
-    src_img = cv2.imread(args.src_img)
-    dst_img = cv2.imread(args.dst_img)
+    src_img = cv2.imread(str(args.src_img), cv2.IMREAD_GRAYSCALE)
 
-    assert src_img.shape == dst_img.shape, 'shape mismatch'
+    # calculate
+    psnrs = []
+    for dst_path in args.dst_img:
+        dst_img = cv2.imread(str(dst_path), cv2.IMREAD_GRAYSCALE)
+        assert src_img.shape == dst_img.shape, 'shape mismatch'
 
-    # to gray
-    src_gray = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
-    dst_gray = cv2.cvtColor(dst_img, cv2.COLOR_BGR2GRAY)
+        divider = src_img.shape[0] * src_img.shape[1]
+        mse = np.power(src_img - dst_img, 2).sum() / divider
 
-    divider = src_img.shape[0] * src_img.shape[1]
-    mse = np.power(src_gray - dst_gray, 2).sum() / divider
+        psnr = 10 * np.log10(255**2 / mse)
+        psnrs.append([int(dst_path.stem.split('_')[-1]), psnr])
 
-    psnr = 10 * np.log10(255**2 / mse)
-    print('PSNR:', np.round(psnr, 4))
+    # draw
+    psnrs.sort()
+    plt.title('PSNR for ' + args.src_img.name)
+    plt.xlabel('Iterations')
+    plt.ylabel('PSNR')
+    plt.grid(True)
+    plt.plot([x[0] for x in psnrs], [x[1] for x in psnrs])
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
